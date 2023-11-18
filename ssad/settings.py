2f973f2ddcfd0afe0446ebb422c9e277dbcb2eda
josef <youcefhaciane24@gmail.com>
2023-11-18 13:29:16 +0100

    delete stat

--- ssad/settings.py
+++ ssad/settings.py
@@ -126,8 +126,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
