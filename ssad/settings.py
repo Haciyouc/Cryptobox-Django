9cbdb718ae841cef95ee0146415ccb61df968d0d
josef <youcefhaciane24@gmail.com>
2023-11-18 13:31:19 +0100

    change stat

--- ssad/settings.py
+++ ssad/settings.py
@@ -126,11 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-
-
 
 
 # Default primary key field type
